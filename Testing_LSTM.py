e64e7174ae470635c3889aea54c2614dd88c814f
abragagn <alberto.bragagnolo@pd.infn.it>
2019-04-04 13:39:17 +0200

    update

--- Testing_LSTM.py
+++ Testing_LSTM.py
@@ -5,7 +5,7 @@ import keras
 from keras.models import Sequential, Model, load_model
 from keras.layers import Dense, Dropout, merge, LSTM, Input
 from keras.layers.normalization import BatchNormalization
-import matplotlib, json, glob
+import matplotlib, json, glob, math
 import numpy as np
 matplotlib.use("Agg")
 import matplotlib.pyplot as plt
@@ -16,25 +16,27 @@ np.random.seed(seed) #init for reproducibilty
 
 ##Input
 maxtracks_read = 100 # max number of tracks to read
-maxtracks_train = 20 # max number of tracks to use in the training 
+maxtracks_train = 25 # max number of tracks to use in the training 
 
 filename = 'ntuHevjin.root'
 file=TFile(filename, 'R')
 tree=file.Get('PDsecondTree')
 
-evtcuts = '((evtNumber % 10) < 8)'
+evtcuts = '((evtNumber % 10) >= 8)' # leave out 20% of events for testing
 
-vInput=root_numpy.tree2array(tree, branches=['trkPt', 'trkEta', 'trkPhi', 'trkIsInJet', 'trkIsHighPurity', 'trkCharge'], selection=evtcuts)
+vInput=root_numpy.tree2array(tree, branches=['trkPt', 'trkEta', 'trkPhi', 'trkDxy', 'trkDz', 'trkIsInJet', 'trkIsHighPurity', 'trkCharge'], selection=evtcuts)
 vInput=root_numpy.rec2array(vInput)
 
-nspec = 2
+nspec = 2 # numebr of feature not used in the training
 nfeat = len(vInput[0]) - nspec
 
 vPt = vInput[:,0]
 vEta = vInput[:,1]
 vPhi = vInput[:,2]
-vtrkIsInJet = vInput[:,3]
-vtrkIsHighPurity = vInput[:,4]
+vDxy = vInput[:,3]
+vDz = vInput[:,4]
+vtrkIsInJet = vInput[:,-3] 
+vtrkIsHighPurity = vInput[:,-2]
 vQ = vInput[:,-1]
 
 ##Shape formatting and zero padding
@@ -44,22 +46,31 @@ for i in range(len(vPt)):
     for j in range(len(vPt[i])):
         if j >= maxtracks_read:
             break
-        if vtrkIsInJet == 0 or vtrkIsHighPurity == 0 :
+        ##Tracks selection
+        if vtrkIsInJet[i][j] == 0 or vtrkIsHighPurity[i][j] == 0 :
+            continue
+        if abs(vDz[i][j]) > 1.0:
+            continue
+        if math.isnan(vDxy[i][j]):
             continue
         vInput[i][j][0] = vPt[i][j]
         vInput[i][j][1] = vEta[i][j]
         vInput[i][j][2] = vPhi[i][j]
+        vInput[i][j][3] = vDxy[i][j]
+        vInput[i][j][4] = vDz[i][j]
         vInput[i][j][-1] = vQ[i][j]
 
-vInput.view('f8,f8,f8,f8').sort(order=['f0'], axis=1) #Ordering by Pt
+vInput.view('f8,f8,f8,f8,f8,f8').sort(order=['f0'], axis=1) #Ordering by increasing Pt (Leonardo's magic)
 
 vInput = vInput[:,-maxtracks_train:,:] #Only using the most energetic maxtracks_train tracks to train
 
+##Labels
 vLabel=root_numpy.tree2array(tree, branches=['ssbLund'], selection=evtcuts)
 vLabel=root_numpy.rec2array(vLabel)
 vLabel[vLabel == 531] = 1
 vLabel[vLabel == -531] = 0
 
+##Weights
 vWeights=root_numpy.tree2array(tree, branches=['evtWeight'], selection=evtcuts)
 vWeights=root_numpy.rec2array(vWeights)
 vWeights=vWeights.reshape((vWeights.shape[0],))
@@ -71,17 +82,37 @@ model = load_model('testLSTM.h5')
 
 print '---- model loaded ----'
 
-pred = model.predict(vInput)
-pred[pred>=0.5] = 1
-pred[pred<0.5] = 0
+score = model.predict(vInput)
+score = score.reshape(len(score))
+pred = np.copy(score)
+pred[pred >= 0.5 ] = 1
+pred[pred < 0.5 ] = 0
+nevt = len(pred)
+sumweight = 0
+for i in vWeights:
+    sumweight = sumweight + i
 
-rt = 0.
-wt = 0.
-for i in range(len(pred)):
+rt = 0
+wt = 0
+for i in range(nevt):
     if pred[i] == vLabel[i]:
-        rt = rt + 1
+        rt =  rt + vWeights[i]
     else:
-        wt = wt + 1
+        wt = wt + vWeights[i]
+
+avgW = float(wt)/(float(rt+wt))
+avgWerr = (TEfficiency.AgrestiCoull(float(rt+wt), float(wt), 0.687, 1)-TEfficiency.AgrestiCoull(float(rt+wt), float(wt), 0.687, 0) )/2
+
+totD2 = 0.
+for i in range(nevt):
+    if score[i] >= 0.5:
+        w = 1 - score[i]
+    else:
+        w = score[i]
+    totD2 += vWeights[i]*((1 - 2*w)**2 ) / sumweight
 
 test_loss, test_acc = model.evaluate(vInput, vLabel)
-print('w from acc: %.2f%%' % ((1-test_acc)*100))
+print('avgw from acc (bias not adressed: %.2f%%' % ((1-test_acc)*100))
+print('avgw from count: (%.2f +- %.2f)%%' % (avgW*100 , avgWerr*100))
+print('avgD2 from count: %.4f' % ((1 - 2*avgW)**2))
+print('avgD2 from per-event: %.4f' % (totD2))
--- Training_LSTM.py
+++ Training_LSTM.py
@@ -5,36 +5,38 @@ import keras
 from keras.models import Sequential, Model
 from keras.layers import Dense, Dropout, merge, LSTM, Input
 from keras.layers.normalization import BatchNormalization
-import matplotlib, json, glob
+import matplotlib, json, glob, math
 import numpy as np
 matplotlib.use("Agg")
 import matplotlib.pyplot as plt
 from matplotlib.colors import LogNorm
 from random import randint
-seed = 7
+seed = 10
 np.random.seed(seed) #init for reproducibilty
 
 ##Input
 maxtracks_read = 100 # max number of tracks to read
-maxtracks_train = 20 # max number of tracks to use in the training 
+maxtracks_train = 25 # max number of tracks to use in the training 
 
 filename = 'ntuHevjin.root'
 file=TFile(filename, 'R')
 tree=file.Get('PDsecondTree')
 
-evtcuts = '((evtNumber % 10) < 8)'
+evtcuts = '((evtNumber % 10) < 8)' # leave out 20% of events for testing
 
-vInput=root_numpy.tree2array(tree, branches=['trkPt', 'trkEta', 'trkPhi', 'trkIsInJet', 'trkIsHighPurity', 'trkCharge'], selection=evtcuts)
+vInput=root_numpy.tree2array(tree, branches=['trkPt', 'trkEta', 'trkPhi', 'trkDxy', 'trkDz', 'trkIsInJet', 'trkIsHighPurity', 'trkCharge'], selection=evtcuts)
 vInput=root_numpy.rec2array(vInput)
 
-nspec = 2
+nspec = 2 # numebr of feature not used in the training
 nfeat = len(vInput[0]) - nspec
 
 vPt = vInput[:,0]
 vEta = vInput[:,1]
 vPhi = vInput[:,2]
-vtrkIsInJet = vInput[:,3]
-vtrkIsHighPurity = vInput[:,4]
+vDxy = vInput[:,3]
+vDz = vInput[:,4]
+vtrkIsInJet = vInput[:,-3] 
+vtrkIsHighPurity = vInput[:,-2]
 vQ = vInput[:,-1]
 
 ##Shape formatting and zero padding
@@ -44,29 +46,39 @@ for i in range(len(vPt)):
     for j in range(len(vPt[i])):
         if j >= maxtracks_read:
             break
-        if vtrkIsInJet == 0 or vtrkIsHighPurity == 0 :
+        ##Tracks selection
+        if vtrkIsInJet[i][j] == 0 or vtrkIsHighPurity[i][j] == 0 :
             continue
+        if abs(vDz[i][j]) > 1.0:
+            continue
+        if math.isnan(vDxy[i][j]) :
+            continue
+
         vInput[i][j][0] = vPt[i][j]
         vInput[i][j][1] = vEta[i][j]
         vInput[i][j][2] = vPhi[i][j]
+        vInput[i][j][3] = vDxy[i][j]
+        vInput[i][j][4] = vDz[i][j]
         vInput[i][j][-1] = vQ[i][j]
 
-vInput.view('f8,f8,f8,f8').sort(order=['f0'], axis=1) #Ordering by Pt
+vInput.view('f8,f8,f8,f8,f8,f8').sort(order=['f0'], axis=1) #Ordering by increasing Pt (Leonardo's magic)
 
 vInput = vInput[:,-maxtracks_train:,:] #Only using the most energetic maxtracks_train tracks to train
 
+##Labels
 vLabel=root_numpy.tree2array(tree, branches=['ssbLund'], selection=evtcuts)
 vLabel=root_numpy.rec2array(vLabel)
 vLabel[vLabel == 531] = 1
 vLabel[vLabel == -531] = 0
 
+##Weights
 vWeights=root_numpy.tree2array(tree, branches=['evtWeight'], selection=evtcuts)
 vWeights=root_numpy.rec2array(vWeights)
 vWeights=vWeights.reshape((vWeights.shape[0],))
 
 ##Model Definition 
-dropoutRate = 0.1
-lstmOutDim = 20
+dropoutRate = 0.5
+lstmOutDim = 50
 
 Inputs = Input(shape=(maxtracks_train,nfeat)) # maxtracks_train, nfeat features
 
@@ -76,7 +88,11 @@ x = Dense(lstmOutDim, activation='relu',kernel_initializer='lecun_uniform')(x)
 if dropoutRate != 0 :
     x = Dropout(dropoutRate)(x)
 
-x = Dense(20, activation='relu',kernel_initializer='lecun_uniform')(x)
+x = Dense(50, activation='relu',kernel_initializer='lecun_uniform')(x)
+if dropoutRate != 0 :
+    x = Dropout(dropoutRate)(x)
+
+x = Dense(25, activation='relu',kernel_initializer='lecun_uniform')(x)
 if dropoutRate != 0 :
     x = Dropout(dropoutRate)(x)
 
@@ -93,14 +109,14 @@ model.summary()
 ##Training model
 reduce_lr = keras.callbacks.ReduceLROnPlateau(monitor='val_loss'
     , factor=0.2
-    , patience=3
-    , min_lr=0.001
+    , patience=5
+    , min_lr=0.0001
     )
 
 history = model.fit(vInput, vLabel
     , sample_weight = vWeights
-    , batch_size=128
-    , epochs=5
+    , batch_size=256
+    , epochs=50
     , verbose=1
     , callbacks=[reduce_lr]
     , validation_split=0.2
@@ -110,7 +126,9 @@ model.save('testLSTM.h5')
 
 scores = model.evaluate(vInput, vLabel)
 print('\n%s: %.2f%%' % (model.metrics_names[1], scores[1]*100))
-print('w: %.2f%%' % ((1-scores[1])*100))
+w = 1-scores[1]
+print('w: %.2f%%' % (w*100))
+print('D^2: %.2f%%' % ( (1-2*w)*(1-2*w)*100 ))
 
 # list all data in history
 print(history.history.keys())
@@ -121,6 +139,16 @@ plt.title('model loss')
 plt.ylabel('loss')
 plt.xlabel('epoch')
 plt.legend(['train', 'test'], loc='upper left')
-plt.savefig('png/'+'plot_history.png')
+plt.savefig('png/'+'plot_loss_history.png')
+plt.clf()
+
+# summarize history for acc
+plt.plot(history.history['acc'])
+plt.plot(history.history['val_acc'])
+plt.title('model acc')
+plt.ylabel('acc')
+plt.xlabel('epoch')
+plt.legend(['train', 'test'], loc='upper left')
+plt.savefig('png/'+'plot_acc_history.png')
 plt.clf()
 
